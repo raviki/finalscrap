7b122cb03ead41917a03d0dba395b54b3b2cdac4
Ravi Kishor <p11ravikishorr@iimahd.ernet.in>
2013-11-26 14:08:43 +0530

    to solve css recognising issue  on production

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,5 @@ module Website
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
     
-    config.assets.initialize_on_precompile = false
   end
 end
