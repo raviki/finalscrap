f0c7a844796979a1c85ed6ba36c953c2ead2d81e
Ravi Kishor <p11ravikishorr@iimahd.ernet.in>
2013-12-23 11:14:24 +0530

    Add update items

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,18 @@ module ApplicationHelper
     I18n.t(:company_phone)
   end 
   
+  def meta_keywords_default
+    I18n.t(:meta_keywords)
+  end
+  
+  def meta_description_default
+    I18n.t(:meta_description)
+  end
+  
+  def title_default
+    I18n.t(:title_default)
+  end 
+  
   Type = {
     TASK: 'task',
     TOOL: 'tool',
